9c967e96195d80ee1d33cfe38da80d794172d8ba
DarielRR <dariel.rojas@ucr.ac.cr>
2024-09-02 17:28:19 -0600

    MoverObjeto

---
+++ src/main/java/ucr/ac/cr/tm2100/g3/moveobject/MoveObject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.tm2100.g3.moveobject;
+
+import ucr.ac.cr.tm2100.g3.moveobject.view.GUIFrame;
+
+/**
+ *
+ * @author Darojas
+ */
+public class MoveObject {
+
+    public static void main(String[] args) {
+        //GUIFrame guiFrame=new GUIFrame(200, 200);
+        GUIFrame guiFrame2=new GUIFrame();
+    }
+}
---
+++ src/main/java/ucr/ac/cr/tm2100/g3/moveobject2/MoveObject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.tm2100.g3.moveobject2;
+
+/**
+ *
+ * @author Darojas
+ */
+public class MoveObject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
